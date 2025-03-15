6581408b3476e2befb86c9ee44d10312ec6a01eb
Mykyta Sikachyna <sikachyna.n@gmail.com>
2025-03-15 18:22:32 +0200

    feat: add the 'toggle with left-click' preference

--- src/preferences.rs
+++ src/preferences.rs
@@ -4,6 +4,7 @@ const DB_NAME: &str = "preferences.redb";
 const TABLE: TableDefinition<&str, bool> = TableDefinition::new("preferences");
  
 pub const PREF_RUN_ACTIVATED: &str = "run_activated";
+pub const PREF_TOGGLE_WITH_LEFT_CLICK: &str = "activate_on_left_click";
 
 pub struct Preferences {
     db: Database
@@ -25,6 +26,9 @@ pub struct Preferences {
     pub fn init(&self) -> Result<(), DBError> {
         if !self.exists(PREF_RUN_ACTIVATED).unwrap() {
             self.save_preference(PREF_RUN_ACTIVATED, true)?;
+        }
+        if !self.exists(PREF_TOGGLE_WITH_LEFT_CLICK).unwrap() {
+            self.save_preference(PREF_TOGGLE_WITH_LEFT_CLICK, true)?;
         }
 
         Ok(())
